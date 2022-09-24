7d0abed61d5d175fc589201572f0fe29780fc30e
Samkayzeee <lasisiabdulsamad9@gmail.com>
2022-09-24 23:31:29 +0100

    worked on signup and login

--- src/pages/Confirmation/Login.jsx
+++ src/pages/Confirmation/Login.jsx
@@ -1,29 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import image from '../../assets/blog4.jpg'
-import Signup from '../Confirmation/Signup';
+import image from '../../assets/blog4.jpg';
+import axios from 'axios';
 
 
 
 const Login = () => {
+
+  const serverUrl = "http://localhost:4040/login";
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function Login(e) {
+        
+      try {
+          e.preventDefault()
+      let loginData = {
+              email:email,
+              password:password
+          }
+          
+          if (email == "" ||password == "" ) {
+              loginData = null;
+              console.log("Input Something Meaningful");
+              console.log(loginData);
+          }
+          else{
+              const response = await axios.post(serverUrl, loginData);
+              console.log(response.data);
+              console.log(loginData);
+          }
+      } catch (error) {
+        console.log(error);  
+      }
+  }
+
   return (
     <div className='divmain'>
       <div className='submain shadow'>
         <div className='div1'style={{backgroundImage: `url(${image})`}} ></div>
 
         <div className='div2'>
-      <form action="">
+      <form action="" onSubmit={Login}>
           <div className='login-container'>
             <h5>LOG IN</h5>
             
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="Password"/>
-      <label for="floatingPassword">Email Address</label>
+    <div className="form-floating">
+      <input onChange={(e) => { setEmail(e.target.value)}} type="email" className="form-control" id="floatingInput" placeholder="Password"/>
+      <label htmlFor="floatingPassword">Email Address</label>
     </div>
 
-    <div class="form-floating  mt-3 ">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="name@example.com"/>
-        <label for="floatingInput">Password</label>
+    <div className="form-floating  mt-3 ">
+        <input onChange={(e) => { setPassword(e.target.value)}} type="password" className="form-control" id="floatingPassword" placeholder="name@example.com"/>
+        <label htmlFor="floatingInput">Password</label>
     </div>
              
     <div className='div-register'>
--- src/pages/Confirmation/Signup.jsx
+++ src/pages/Confirmation/Signup.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
-
-import image from '../../assets/blog4.jpg'
-
-import { Link } from 'react-router-dom'
-import Login from './Login'
+import React, { useState } from 'react';
+import image from '../../assets/blog4.jpg';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Signup = () => {
+
+      const serverUrl = "http://localhost:4040/signup";
+      const [fullname, setFullname] = useState("");
+      const [email, setEmail] = useState("");
+      const [password, setPassword] = useState("");
+      const [username, setUsername] = useState("");
+
+
+      async function Signup(e) {
+        
+        try {
+            e.preventDefault()
+        let signData = {
+                fullname:fullname,
+                email:email,
+                password:password,
+                username:username
+            }
+            
+            if (fullname == "" ||email == "" || password == "" || username == "") {
+                signData = null;
+                console.log("Input Something Meaningful");
+                console.log(signData);
+            }
+            else{
+                const response = await axios.post(serverUrl, signData);
+                console.log(response.data);
+                console.log(signData);
+            }
+        } catch (error) {
+          console.log(error);  
+        }
+    }      
+
+
   return (
     <div className='divmain'>
       <div className='submain shadow'>
@@ -16,25 +49,25 @@ const Signup = () => {
           <h3 className='reg'>Registration Form</h3>
           <div className='divsign container'>
             <h5>SIGN UP</h5>
-        <form action="">
-<div class="form-floating mb-3">
-  <input type="password" class="form-control" id="floatingInput" placeholder="name@example.com"/>
-  <label for="floatingInput">Full Name</label>
+        <form action="" onSubmit={Signup}>
+<div className="form-floating mb-3">
+  <input onChange={(e) => { setFullname(e.target.value)}} type="text" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+  <label htmlFor="floatingInput">Full Name</label>
 </div>
 
-<div class="form-floating">
-  <input type="email" class="form-control" id="floatingPassword" placeholder="Password"/>
-  <label for="floatingPassword">Email Address</label>
+<div className="form-floating">
+  <input onChange={(e) => { setEmail(e.target.value)}} type="email" className="form-control" id="floatingPassword" placeholder="Password"/>
+  <label htmlFor="floatingPassword">Email Address</label>
 </div>
 
-<div class="form-floating  mt-3 ">
-  <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com"/>
-  <label for="floatingInput">Create Username</label>
+<div className="form-floating  mt-3 ">
+  <input onChange={(e) => { setUsername(e.target.value)}} type="text" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+  <label htmlFor="floatingInput">Create Username</label>
 </div>
 
-<div class="form-floating  mt-3 ">
-  <input type="password" class="form-control" id="floatingInput" placeholder="name@example.com"/>
-  <label for="floatingInput">Create Password</label>
+<div className="form-floating  mt-3 ">
+  <input onChange={(e) => { setPassword(e.target.value)}} type="password" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+  <label htmlFor="floatingInput">Create Password</label>
 </div>
 
 <div className='div-register'>
