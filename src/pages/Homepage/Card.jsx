f05d4a395dfaa609d1230f5e3d58041d68f556b4
Timmy <temmyy92@gmail.com>
2022-09-27 12:06:42 +0100

    added navbar

--- src/pages/Homepage/Card.jsx
+++ src/pages/Homepage/Card.jsx
@@ -1,40 +1,48 @@
-import './Homepage.css'
-import image from '../../assets/secondimage.webp'
-import image1 from '../../assets/thirdimage.jpeg'
+import "./Homepage.css";
+import image from "../../assets/secondimage.webp";
+import image1 from "../../assets/thirdimage.jpeg";
 const Card = (props) => {
-    return (<div className='container w-100 justify-content-center gap-5 d-flex flex-wrap'>
-        {props.item.map((value, index) =>
-        
-        <div className="cardd card position-relative rounded-0 mt-5 h-25" style={{ width: 23 + "rem"}} key={index}>
-                <div className='img'>
-                <img src={value.urlToImage} className="card-img-top w-100 h-100 rounded-0" alt="..." />
-           </div>
-            
-            <div className="card-body p-0">
-           
-                <div className='w-100 d-flex py-5 flex-column mt-2 mb-2 p-2 text-center'>
-                    <div className="circle shadow rounded-circle position-absolute start-50 translate-middle">
-                        <img src={image1} className='w-75 h-75 rounded-circle '/>
-                </div>
-                        <div className='title overflow-auto'>
-                        <h5 className="card-title">{value.title}</h5>
-                       </div>
-   </div>
-                <div className='but1 w-100 d-flex justify-content-between '>
-                    <button className='but'><i className="fa-solid fa-heart px-3"></i> 57</button>
-                    <button className='but'><i className="fa-solid fa-comment px-3"></i> 6</button>
-                    <button className='but'><i className="fa-solid fa-eye px-3"></i> 138</button>
-    </div>
-  </div>
-</div>
-        )
+	return (
+		<div className="container w-100 justify-content-center gap-5 d-flex flex-wrap">
+			{props.item.map((value, index) => (
+				<div
+					className="cardd card position-relative rounded-0 mt-5 h-25"
+					style={{ width: 23 + "rem" }}
+					key={index}
+				>
+					<div className="img">
+						<img
+							src={value.urlToImage}
+							className="card-img-top w-100 h-100 rounded-0"
+							alt="..."
+						/>
+					</div>
 
+					<div className="card-body p-0">
+						<div className="w-100 d-flex py-5 flex-column mt-2 mb-2 p-2 text-center position-relative ">
+							<div className="circle shadow rounded-circle position-absolute start-50 translate-middle top-0">
+								<img src={image1} className="w-75 h-75 rounded-circle " />
+							</div>
+							<div className="title overflow-auto">
+								<h5 className="card-title">{value.title}</h5>
+							</div>
+						</div>
+						<div className="but1 w-100 d-flex justify-content-between ">
+							<button className="but">
+								<i className="fa-solid fa-heart px-3"></i> 57
+							</button>
+							<button className="but">
+								<i className="fa-solid fa-comment px-3"></i> 6
+							</button>
+							<button className="but">
+								<i className="fa-solid fa-eye px-3"></i> 138
+							</button>
+						</div>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
 
-
-        }
-       
-    
-    </div> );
-}
- 
 export default Card;
--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,7 +1,56 @@
-import './Navbar.css'
+import "./Navbar.css";
 const Navbar = () => {
-    return (
-        <nav className="nav d-flex w-100 align-items-center shadow-sm">
+	return (
+		<>
+			<nav class="navbar navbar-expand-lg bg-light">
+				<div class="container-fluid">
+					<a class="navbar-brand" href="#">
+						BONA
+					</a>
+					<button
+						class="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav ms-4 me-auto mb-2 mb-lg-0">
+							<li class="nav-item">
+								<a class="nav-link active" aria-current="page" href="#">
+									Home
+								</a>
+							</li>
+							<li class="nav-item ms-4">
+								<a class="nav-link" href="#">
+									Categories
+								</a>
+							</li>
+							<li class="nav-item ms-4">
+								<a class="nav-link" href="#">
+									Features
+								</a>
+							</li>
+						</ul>
+						<form class="d-flex w-50" role="search">
+							<input
+								class="form-control me-2"
+								type="search"
+								placeholder="Type of search"
+								aria-label="Search"
+							/>
+							{/* <button class="btn btn-outline-success" type="submit">
+								Search
+							</button> */}
+						</form>
+					</div>
+				</div>
+			</nav>
+			{/* <nav className="nav d-flex w-100 align-items-center shadow-sm">
             <div className='w-50'>
             <button>Bona</button>
             <button>Home</button>
@@ -12,9 +61,9 @@ const Navbar = () => {
             <div className='w-50'>
                <input type="search" placeholder='Type of search' className='form-control w-100 rounded-0'/>
             </div>
-        </nav>
+        </nav> */}
+		</>
+	);
+};
 
-    );
-}
- 
 export default Navbar;
